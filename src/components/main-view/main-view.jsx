774047651876ef9699bf91d20b165591dfe45dc9
Doug Hill <stix2you@gmail.com>
2024-03-04 15:57:56 -0600

    .

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,60 +8,60 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
 
 export const MainView = () => {
-    const [books, setBooks] = useState([]);
-    // const [selectedBook, setSelectedBook] = useState(null);
-    const [user, setUser] = useState(null);
+   const [books, setBooks] = useState([]);
+   // const [selectedBook, setSelectedBook] = useState(null);
+   const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        fetch("https://openlibrary.org/search.json?q=star+wars")
-            .then((response) => response.json())
-            .then((data) => {
-                const booksFromApi = data.docs.map((doc) => {
-                    return {
-                        id: doc.key,
-                        title: doc.title,
-                        image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
-                        author: doc.author_name?.[0]
-                    };
-                });
-
-                setBooks(booksFromApi);
+   useEffect(() => {
+      fetch("https://openlibrary.org/search.json?q=star+wars")
+         .then((response) => response.json())
+         .then((data) => {
+            const booksFromApi = data.docs.map((doc) => {
+               return {
+                  id: doc.key,
+                  title: doc.title,
+                  image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+                  author: doc.author_name?.[0]
+               };
             });
-    }, []);
 
-    return (
-        <Container>
-            <BrowserRouter>
-                <NavigationBar user={user} onLoggedOut={() => setUser(null)} />
-                <Row className="justify-content-md-center">
-                    <Routes>
-                        <Route path="/signup" element={<>{user ? (<Navigate to="/" />) : (<Col md={5}><SignupView /></Col>
-                        )}</>    // navigate to the signup view if the user is not logged in
-                        }
-                        />
+            setBooks(booksFromApi);
+         });
+   }, []);
 
-                        <Route path="/login" element={<>{user ? (<Navigate to="/" />) : (<Col md={5}><LoginView onLoggedIn={(user) => setUser(user)} />
-                        </Col>   // navigate to the login view if the user is not logged in
-                        )}</>
-                        }
-                        />
+   return (
+      <Container>
+         <BrowserRouter>
+            <NavigationBar user={user} onLoggedOut={() => setUser(null)} />
+            <Row className="justify-content-md-center">
+               <Routes>
+                  <Route path="/signup" element={<>{user ? (<Navigate to="/" />) : (<Col md={5}><SignupView /></Col>
+                  )}</>    // navigate to the signup view if the user is not logged in
+                  }
+                  />
 
-                        <Route path="/books/:bookId" element={<>{!user ? (<Navigate to="/login" replace />) : (<Col md={8}><BookView books={books} />
-                        </Col>  // navigate to the book view if the user is logged in
-                        )}</>
-                        }
-                        />
+                  <Route path="/login" element={<>{user ? (<Navigate to="/" />) : (<Col md={5}><LoginView onLoggedIn={(user) => setUser(user)} />
+                  </Col>   // navigate to the login view if the user is not logged in
+                  )}</>
+                  }
+                  />
 
-                        <Route path="/" element={<>{!user ? (<Navigate to="/login" replace />) : books.length === 0 ? (<Col>The list is empty!</Col>
-                        ) : (<>{books.map((book) => (<Col className="mb-4" key={book.id} md={3}><BookCard book={book} />
-                        </Col>  // map over the books and create a card for each one, bookcard component renders the book card
-                        ))}</>
-                        )}</>
-                        }
-                        />
-                    </Routes>
-                </Row>
-            </BrowserRouter>
-        </Container>
-    );
+                  <Route path="/books/:bookId" element={<>{!user ? (<Navigate to="/login" replace />) : (<Col md={8}><BookView books={books} />
+                  </Col>  // navigate to the book view if the user is logged in
+                  )}</>
+                  }
+                  />
+
+                  <Route path="/" element={<>{!user ? (<Navigate to="/login" replace />) : books.length === 0 ? (<Col>The list is empty!</Col>
+                  ) : (<>{books.map((book) => (<Col className="mb-4" key={book.id} md={3}><BookCard book={book} />
+                  </Col>  // map over the books and create a card for each one, bookcard component renders the book card
+                  ))}</>
+                  )}</>
+                  }
+                  />
+               </Routes>
+            </Row>
+         </BrowserRouter>
+      </Container>
+   );
 };
